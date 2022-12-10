16f3ea08272e5082b219347cc25546fba2555bab
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2022-12-11 00:23:43 +0600

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <climits>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#define ll long long int
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvpii vector<vector<pair<int, int>>>
+#define si stack<int>
+#define sll stack<ll>
+#define qi queue<int> 
+#define qll queue<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psll pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pb push_back
+
+const ll mod = 1e9+7;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+    
+    
+}
