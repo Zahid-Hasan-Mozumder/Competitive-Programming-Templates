889ec4dad5dd7e34e1fd287a0f2d462807531c45
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2022-12-28 01:33:16 +0600

    Update Count number of subarray with given xor.cpp

--- Bit Manipulation/Count number of subarray with given xor.cpp
+++ Bit Manipulation/Count number of subarray with given xor.cpp
@@ -1,6 +1,6 @@
 #define ll long long int
 
-ll subarrayXor(vector<int> &a, int n, int m){
+ll subarrayXor(vector<int> a, int n, int m){
     ll ans = 0;
     int cumXor = 0;
     vector<int> mp(4*n, 0);
