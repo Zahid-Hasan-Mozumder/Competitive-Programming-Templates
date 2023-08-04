84118af0ae7a65fe428420026fc725e82115ae26
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-08-04 16:23:03 +0600

    Update Disjoint Set Union(DSU )(Union By Size).cpp

--- Graph Algorithms/Disjoint Set Union(DSU )(Union By Size).cpp
+++ Graph Algorithms/Disjoint Set Union(DSU )(Union By Size).cpp
@@ -5,52 +5,49 @@ using namespace std;
 
 vector<int> size, parent;
 
-void initialize(int n){
+void init(int n){
     size.resize(n+1, 1); 
     parent.resize(n+1);
     for(int i=1; i<=n; i++) parent[i] = i;
 }
 
-int findParent(int u){
-    if(u == parent[u])
-        return u;
-    return parent[u] = findParent(parent[u]);
+int find(int v){
+    if(parent[v] == v)
+        return v;
+    return parent[v] = find(parent[v]);
 }
 
-void unionBySize(int u, int v){
-    int upu = findParent(u);
-    int upv = findParent(v);
-    if(upu == upv) return;
-    if(size[upu] < size[upv]){
-        parent[upu] = upv;
-        size[upv] += size[upu];
-    }
-    else if(size[upu] > size[upv]){
-        parent[upv] = upu;
-        size[upu] += size[upv];
-    }
-    else{
-        parent[upv] = upu;
-        size[upu] += size[upv];
+void Union(int u, int v){
+    int pu = find(u);
+    int pv = find(v);
+    // If u and v are in different group
+    if(pu != pv){
+        // Always keeping the big tree or group at left
+        if(size[pu] < size[pv])
+            swap(pu, pv);
+        // Attaching right smallest group to left biggest group
+        parent[pv] = pu;
+        size[pu] += size[pv];
     }
 }
 
 int main(){
-    int n, q; cin>>n>>q;
-    initialize(n);
+    
+    int n, q; cin >> n >> q;
+    
+    init(n);
+    
     while(q--){
-        int qt; cin>>qt;
+        int qt; cin >> qt;
         // 1 : Add an edge between this two nodes
         // 2 : Does this two nodes belong to the same component
         if(qt == 1){
-            int u, v; cin>>u>>v;
-            unionBySize(u, v);
+            int u, v; cin >> u >> v;
+            Union(u, v);
         }
         else if(qt == 2){
-            int u, v; cin>>u>>v;
-            int upu = findParent(u);
-            int upv = findParent(v);
-            if(upu == upv) cout<< "Belongs to the same component" << '\n';
+            int u, v; cin >> u >> v;
+            if(find(u) == find(v)) cout<< "Belongs to the same component" << '\n';
             else cout<< "Does not belong to the same component" << '\n';
         }
     }
